38dd0aefd2b1d973908951a5090921f29d06534a
Asus <sajibroy206@gmail.com>
2024-10-27 22:48:27 +0600

    add component

--- lib/ui/SplashScreen.dart
+++ lib/ui/SplashScreen.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:getx_mvvm/component/RoundButton.dart';
-
-import '../component/InternetException.dart';
-
-
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
